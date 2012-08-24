d611ca8cc339f253751d177ba282fc547f1589c1
Paul Keller <Paul@Paul-Kellers-iMac.local>
2012-08-24 16:30:43 +0100

    Initial Commit

---
+++ SOS Contact/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  SOS Contact
+//
+//  Created by Paul Keller on 24/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
